df75645ba4fdc22cadf745a3ea7ccea04c7429e3
Ricardo Roman <r.rmn92@gmail.com>
2019-08-15 23:03:14 -0600

    👌 IMPROVE: Create mono-repo

---
+++ back-end/createSemester.js
@@ -0,0 +1,31 @@
+import uuid from "uuid";
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure, unauthorized } from "./libs/response-lib";
+
+export async function main(event, context) {
+  const { cognitoIdentityId } = event.requestContext.identity;
+  if (!cognitoIdentityId) {
+    return unauthorized({ status: false });
+  }
+
+  const data = JSON.parse(event.body);
+  const { semestersTableName } = process.env;
+  const params = {
+    TableName: semestersTableName,
+    Item: {
+      semesterId: uuid.v1(),
+      semesterName: data.semesterName,
+      startDate: data.startDate,
+      endDate: data.endDate,
+      createdAt: Date.now(),
+      updatedAt: null,
+    }
+  };
+
+  try {
+    await dynamoDbLib.call("put", params);
+    return success(params.Item);
+  } catch (e) {
+    return failure({ status: false });
+  }
+}
--- back-end/libs/response-lib.js
+++ back-end/libs/response-lib.js
@@ -6,6 +6,10 @@ export function failure(body) {
   return buildResponse(500, body);
 }
 
+export function unauthorized(body) {
+  return buildResponse(401, body);
+}
+
 function buildResponse(statusCode, body) {
   return {
     statusCode: statusCode,
