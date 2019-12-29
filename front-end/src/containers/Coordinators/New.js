56c39fc7080ed3666172c67c884d6fea88db64d3
Ricardo Roman <r.rmn92@gmail.com>
2019-12-29 10:49:15 -0700

    📦 NEW: Create basic new form. Missing datepicker.

--- front-end/src/containers/Coordinators/New.js
+++ front-end/src/containers/Coordinators/New.js
@@ -1,12 +1,92 @@
 import React, { useState } from 'react';
+import { Form } from 'react-bootstrap';
+import { API } from 'aws-amplify';
+import { useFormFields } from '../../libs/hooksLib';
+import LoaderButton from '../../components/LoaderButton';
 
 export default function NewCoordinator(props) {
   const [isLoading, setIsLoading] = useState(false);
+  const [fields, handleFieldChange] = useFormFields({
+    coordinatorNames: '',
+    lastNames: '',
+    birthdate: '',
+    gender: '',
+  });
+
+  function validateForm() {
+    return (
+      fields.coordinatorNames.length > 0 &&
+      fields.lastNames.length > 0 &&
+      fields.birthdate.length > 0 &&
+      fields.gender.length > 0
+    );
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    setIsLoading(true);
+    try {
+      await createCoordinator(fields); 
+      props.history.push('/coordinators');
+    } catch (error) {
+      alert(error.message);
+      setIsLoading(false);
+    }
+  }
+
+  async function createCoordinator(coordinator) {
+    return API.post('coordinators', '', {
+      body: coordinator,
+    });
+  }
 
   return (
     <div className="pt-2">
       <h1 className="py-2">New Coordinator</h1>
-      {!isLoading && <span>test</span>}
+      <Form onSubmit={handleSubmit}>
+        <Form.Group controlId="coordinatorNames"> 
+          <Form.Label>Names</Form.Label>
+          <Form.Control
+            autoFocus
+            type="text"
+            value={fields.coordinatorNames}
+            onChange={handleFieldChange}
+          />
+        </Form.Group>
+        <Form.Group controlId="lastNames">
+          <Form.Label>Last names</Form.Label>
+          <Form.Control
+            type="text"
+            value={fields.lastNames}
+            onChange={handleFieldChange}
+          />
+        </Form.Group>
+        <Form.Group controlId="gender">
+          <Form.Label>Gender</Form.Label>
+          <Form.Control
+            type="text"
+            value={fields.gender}
+            onChange={handleFieldChange}
+          />
+        </Form.Group>
+        <Form.Group controlId="birthdate">
+          <Form.Label>Birthdate</Form.Label>
+          <Form.Control
+            type="text"
+            value={fields.birthdate}
+            onChange={handleFieldChange}
+          />
+        </Form.Group>
+        <LoaderButton
+          type="submit"
+          block
+          variant="light"
+          isLoading={isLoading}
+          disabled={!validateForm()}
+        >
+          Login
+        </LoaderButton>
+      </Form>
     </div>
   );
 }
